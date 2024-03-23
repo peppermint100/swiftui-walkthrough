74f9122f76b744fa5a42a5feca026e21b97afc1b
peppermint100 <krpeppermint100@gmail.com>
2024-03-23 21:31:32 +0900

    [TakeHomeProject] #21

---
+++ TakeHomeProject/TakeHomeProject/Features/People/ViewModels/PeopleViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  PeopleViewModel.swift
+//  TakeHomeProject
+//
+//  Created by peppermint100 on 3/23/24.
+//
+
+import Foundation
+
+final class PeopleViewModel: ObservableObject {
+    
+    @Published private(set) var users: [User] = []
+    
+    func fetchUsers() {
+        NetworkingManager.shared.request("https://reqres.in/api/users",
+                                         type: UsersResponse.self) { [weak self] res in
+            switch res {
+            case .success(let response):
+                self?.users = response.data
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
--- TakeHomeProject/TakeHomeProject/Features/People/Views/PeopleView.swift
+++ TakeHomeProject/TakeHomeProject/Features/People/Views/PeopleView.swift
@@ -12,7 +12,7 @@ struct PeopleView: View {
     private let columns = Array(
         repeating: GridItem(.flexible()), count: 2)
     
-    @State private var users: [User] = []
+    @StateObject private var vm = PeopleViewModel()
     @State private var shouldShowCreate = false
     
     var body: some View {
@@ -23,7 +23,7 @@ struct PeopleView: View {
                 
                 ScrollView {
                     LazyVGrid(columns: columns, spacing: 16) {
-                        ForEach(users, id: \.id) { user in
+                        ForEach(vm.users, id: \.id) { user in
                             NavigationLink{
                                 DetailView()
                             } label: {
@@ -41,15 +41,7 @@ struct PeopleView: View {
                 }
             }
             .onAppear {
-                NetworkingManager.shared.request("https://reqres.in/api/users",
-                                                 type: UsersResponse.self) { res in
-                    switch res {
-                    case .success(let response):
-                        users = response.data
-                    case .failure(let error):
-                        print(error)
-                    }
-                }
+                vm.fetchUsers()
             }
             .sheet(isPresented: $shouldShowCreate) {
                 CreateView()
