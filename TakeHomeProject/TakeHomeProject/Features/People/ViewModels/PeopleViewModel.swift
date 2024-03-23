732af3b6161042b79d7280e559209cf996da37cd
peppermint100 <krpeppermint100@gmail.com>
2024-03-23 21:34:00 +0900

    [TakeHomeProject] #22

--- TakeHomeProject/TakeHomeProject/Features/People/ViewModels/PeopleViewModel.swift
+++ TakeHomeProject/TakeHomeProject/Features/People/ViewModels/PeopleViewModel.swift
@@ -14,11 +14,13 @@ final class PeopleViewModel: ObservableObject {
     func fetchUsers() {
         NetworkingManager.shared.request("https://reqres.in/api/users",
                                          type: UsersResponse.self) { [weak self] res in
-            switch res {
-            case .success(let response):
-                self?.users = response.data
-            case .failure(let error):
-                print(error)
+            DispatchQueue.main.async {
+                switch res {
+                case .success(let response):
+                    self?.users = response.data
+                case .failure(let error):
+                    print(error)
+                }
             }
         }
     }
