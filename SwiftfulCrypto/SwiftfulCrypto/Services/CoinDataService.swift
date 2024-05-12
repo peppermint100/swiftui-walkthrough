501040db6886d3d019280bdd0ecd2333eac8ded6
peppermint100 <krpeppermint100@gmail.com>
2024-05-12 21:44:53 +0900

    crypto api key

--- SwiftfulCrypto/SwiftfulCrypto/Services/CoinDataService.swift
+++ SwiftfulCrypto/SwiftfulCrypto/Services/CoinDataService.swift
@@ -19,7 +19,7 @@ class CoinDataService {
     
     private func getCoins() {
         guard let url = URL(
-            string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
+            string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h&x_cg_demo_api_key=\(KeyConstant.apiKey)")
         else { return }
         
         coinSubscription = NetworkingManager.download(url: url)
